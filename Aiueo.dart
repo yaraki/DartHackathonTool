99e582eebbf81c1259a9dfff6703c2e85fe1c8c7
Keiji Ariyama <keiji_ariyama@c-lis.co.jp>
2012-05-12 22:18:23 +0900

    WebRTCが有効化されていない場合、エラーメッセージを表示するようにした。

--- Aiueo.dart
+++ Aiueo.dart
@@ -7,7 +7,7 @@ class Camera {
   var localCanvas;
   var ctx2;
   var prevImg = null;
-  final int threshold = 2900000;
+  final int threshold = 3200000;
   
   void run() {
     localVideo = document.query("#selfView");
@@ -25,23 +25,34 @@ class Camera {
     }
   }
   
-  void toggle() {
-    print("Hello");
-  }
-  
   void startCamera() {
     try {
       window.navigator.webkitGetUserMedia("video user", gotStream);
+      
+      // Detect MediaStream disabled.
+      window.setTimeout(detectMediaStreamDisabled, 2 * 1000);
+      
     } catch (var e) {
       print(e);
     }
   }
   
+  bool started = false;
+  
   void gotStream(var stream) {
+    started = true;
     localVideo.src = new dom.DOMURL().createObjectURL(stream);
     startCanvasCopy();
   }
-  
+
+  void detectMediaStreamDisabled() {
+    if (started) {
+      return ;
+    }
+    
+    document.query("#error").style.visibility = "visible";
+  }
+
   void startCanvasCopy() {
     canvasCopy(100);
   }
@@ -79,12 +90,13 @@ class Camera {
   var prevDiff = 0;
   
   void renderCanvas() {
-    ctx2.drawImage(localVideo, 0, 0, 200, 200); var imgd = ctx2.getImageData(0, 0, localCanvas.width, localCanvas.height);
+    ctx2.drawImage(localVideo, 0, 0, 200, 200);
+    var imgd = ctx2.getImageData(0, 0, localCanvas.width, localCanvas.height);
     binaryImage(imgd);
     if (prevImg !== null) {
       int diff = checkDiff(prevImg, imgd);
       if (prevDiff > 0 && diff > threshold) {
-        document.query('#status').innerHTML = "上司検出！" + diff;
+        document.query('#status').innerHTML = "Boss is detected！" + diff;
         bossIsHere();
       } else {
         prevDiff = diff;
